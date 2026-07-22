59666309cf8b3b42b6bf237294342b8b294dde15
OzgurSak5 <ozgur.sak@time.com.tr>
2026-07-22 13:17:16 +0300

    feat(web): add route guard to protect profile page

--- submissions/ozgur-sak-login/web/src/App.tsx
+++ submissions/ozgur-sak-login/web/src/App.tsx
@@ -2,13 +2,17 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Login from './Login'
 import Profile from './Profile'
 import './App.css'
+import ProtectedRoute from './ProtectedRoute'
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/login" element={<Login />} />
-        <Route path="/profile" element={<Profile />} />
+        <Route path="/profile" element={
+          <ProtectedRoute>
+            <Profile />
+          </ProtectedRoute>}/>
         <Route path="/" element={<Login />} />
       </Routes>
     </BrowserRouter>
